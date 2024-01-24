c565039deb95e6c7432200143d3e88c9a144cc7f
SakuraJinkyu <122227601+SakuraJinkyu@users.noreply.github.com>
2024-01-23 20:11:01 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+
+
+function setHalfVolume() {
+    var myAudio = document.getElementById("audio1");  
+    myAudio.volume = 0.2; //Changed this to 0.5 or 50% volume since the function is called Set Half Volume ;)
+}
