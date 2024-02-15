d9364f90b2004a577595dc81990d29b4a466a37d
SakuraJinkyu <122227601+SakuraJinkyu@users.noreply.github.com>
2024-02-14 20:39:29 -0500

    Update index.js
    Autoplay needs to start muted. 
    Updating to remove changes.

--- index.js
+++ index.js
@@ -5,18 +5,5 @@ function setHalfVolume() {
     myAudio.volume = 0.1;
 }
 
-// Test code to play audio for Github Pages (Java issue?)
-  var hasPlayed = false;
-  var audio = new Audio(
-    "https://raw.githubusercontent.com/SakuraJinkyu/Nikke-and-Genshin-Pulling/audio/genshin-bgm.mp3"
-  );
-  function playAudio() {
-    // This func will check if the file hasPlayed and then will disable after first interaction.
-    if (!hasPlayed) {
-      audio.play();
-      hasPlayed = true;
-    }
-  }
-  document.addEventListener("click", playAudio);
 
 // Set a onclick function for the two Nav's (current plan, change color to blue, and add a hover)
