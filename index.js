58d9e5a667358fab5eb768702b206a79ec71db95
SakuraJinkyu <122227601+SakuraJinkyu@users.noreply.github.com>
2024-02-21 18:37:19 -0500

    Update index.js
    Reverting to original. Need to do more testing with audio setup on GitHub.

--- index.js
+++ index.js
@@ -7,24 +7,8 @@ function setHalfVolume() {
 
 // Set a onclick function for the two Nav's (current plan, change color to blue, and add a hover)
 
-// Testing toggle JS function; Create const for what's being queried, then can use aria-pressed in HTML to trigger button (if, else to set the audio to pause or play). 
-const toggleBtn = document.querySelector("#button-audio");
-const genshinbgm = document.querySelector("audio1")
-
-// listen for when the button is clicked, and call toggle when so
-toggleBtn.addEventListener("click", toggle);
-
-function toggle(){
-  // always unmute the audio
-  audio.muted = false;
-
-  // is the button currently 'pressed'?
-  const pressed = toggleBtn.getAttribute("aria-pressed") === "true";
-  
-  // toggle the audio based-on the state of the button
-  if(pressed) audio.pause();
-  else        audio.play();
-
-  // update the state of the button
-  toggleBtn.setAttribute("aria-pressed", !pressed);
-}// toggle
+// If this doesn't work, will need to do more research on javascript audio setups (possibly an if else statement)
+function playMusic() {
+    var genshinbgm = document.getElementById("audio1");
+    genshinbgm.muted = !genshinbgm.muted;
+}
