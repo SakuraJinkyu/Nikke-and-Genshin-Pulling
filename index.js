dfe106adda4e666dc5f0c0afc4cff097d3bc28c1
SakuraJinkyu <122227601+SakuraJinkyu@users.noreply.github.com>
2024-02-26 18:35:27 -0500

    Update index.js
    Added new audio script that seems to work more consistently.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
-
+// Use to set volume; Keeping it quiet for now so it won't blast eardrums
 function setHalfVolume() {
     var myAudio = document.getElementById("audio1");  
     myAudio.volume = 0.1;
 }
 
-// Set a onclick function for the two Nav's (current plan, change color to blue, and add a hover)
 
-// If this doesn't work, will need to do more research on javascript audio setups (possibly an if else statement)
-function playMusic() {
-    var genshinbgm = document.getElementById("audio1");
-    genshinbgm.muted = !genshinbgm.muted;
-}
+
+// ? functionality (condition to test ? value if true : value if false; creates an ifelse statement); myAudio.pause must be set t pause, not play (else audio will not pause)o
+function toggleBGM() {
+    var myAudio = document.getElementById("audio1");
+    return myAudio.paused ? myAudio.play() : myAudio.pause();
+};
