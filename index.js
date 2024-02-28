1e033da69aa2e62851bd81dc60af55b2de2d85b2
SakuraJinkyu <122227601+SakuraJinkyu@users.noreply.github.com>
2024-02-28 18:18:43 -0500

    Update index.js
    Updated with new Nikke music, and set volume for both BGM's to be at a reasonable volume.

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 
-// Use to set volume; Keeping it quiet for now so it won't blast eardrums
+// Use to set volume; Updated to add Nikke audio
 function setHalfVolume() {
-    var myAudio = document.getElementById("audio1");  
-    myAudio.volume = 0.1;
+    var gameAudio = document.getElementsByClassName("audioVolume");
+    gameAudio[0].volume = 0.1;
+    gameAudio[1].volume = 0.1;
 }
 
 
 
 // ? functionality (condition to test ? value if true : value if false; creates an ifelse statement); myAudio.pause must be set t pause, not play (else audio will not pause)o
-function toggleBGM() {
-    var myAudio = document.getElementById("audio1");
-    return myAudio.paused ? myAudio.play() : myAudio.pause();
+function toggleGenshin() {
+    var genshinAudio = document.getElementById("audio-genshin");
+    return genshinAudio.paused ? genshinAudio.play() : genshinAudio.pause();
+};
+
+function toggleNikke() {
+    var nikkeAudio = document.getElementById("audio-nikke");
+    return nikkeAudio.paused ? nikkeAudio.play() : nikkeAudio.pause();
 };
